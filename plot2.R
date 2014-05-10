021f326ab8742fc96e6ea3b1e4dec9ff9f2b1b18
Arun Kaushik <arundevkauhsik@gmail.com>
2014-05-10 17:31:01 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,41 @@
+
+###############################################   
+# Time Series of Global Active Power#     
+###############################################
+
+### Download the data from https://d396qusza40orc.cloudfront.net/
+###    exdata%2Fdata%2Fhousehold_power_consumption.zip 
+### and then unzipped
+
+work_dir="C:/Users/KAUSHIK/Desktop/Assignment"
+# set working directory to your working folder where data is available
+setwd(work_dir)
+
+# Read data, Its will take 15 seconds
+data <- read.table('household_power_consumption.txt', sep=';', header=T,
+                   colClasses = c('character', 'character', 'numeric',
+                                  'numeric', 'numeric', 'numeric',
+                                  'numeric', 'numeric', 'numeric'),
+                   na.strings='?')
+
+# convert dates and subset on two days in February 2007
+data$DateTime <- strptime(paste(data$Date, data$Time), 
+                          "%d/%m/%Y %H:%M:%S")
+data <- subset(data, 
+               as.Date(DateTime) >= as.Date("2007-02-01") & 
+                   as.Date(DateTime) <= as.Date("2007-02-02"))
+
+# call plot device plot2.png
+png("plot2.png", height=480, width=480)
+
+# Build time series
+plot(data$DateTime, 
+     data$Global_active_power, 
+     pch=NA, 
+     xlab="", 
+     ylab="Global Active Power (kilowatts)")
+lines(data$DateTime, data$Global_active_power)
+
+
+dev.off()  # Close graphic device
+
